82463fe46f377c15e4e46a7ad6f8d91fb2f45266
Phil Marshall <dr.phil.marshall@gmail.com>
2015-01-14 06:19:12 -0800

    Started notebook demo for CtU2015 hack

--- beatbox/universe.py
+++ beatbox/universe.py
@@ -1,10 +1,12 @@
-import numpy
+import numpy as np
 
 class Universe(object):
-
+    """
+    A simple model universe in a box. Coordinates are comoving cartesians!
+    """
     def __init__(self):
         self.size = 100
-        self.phi = np.zeros(3,100)
+        self.phi = np.zeros([3,self.size])
         return
 
     def __str__(self):

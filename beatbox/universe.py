b7558fa0889256cabc0f7a718fc1681ec971a5ba
Phil Marshall <dr.phil.marshall@gmail.com>
2015-08-24 17:16:57 -0700

    Fake potential grid for testing

--- beatbox/universe.py
+++ beatbox/universe.py
@@ -11,12 +11,19 @@ class Universe(object):
     A simple model universe in a box.
     """
     def __init__(self):
-        self.size = 100
-        self.phi = np.zeros([3,self.size])
+        self.PIXSCALE = 0.1
+        self.BOXSIZE = 4.0
+        self.NPIX = int(self.BOXSIZE/self.PIXSCALE) + 1
+        Nj = np.complex(0.0,self.NPIX)
+        self.x, self.y, self.z = np.mgrid[-self.BOXSIZE/2.0:self.BOXSIZE/2.0:Nj, -self.BOXSIZE/2.0:self.BOXSIZE/2.0:Nj, -self.BOXSIZE/2.0:self.BOXSIZE/2.0:Nj]
+        # self.phi = np.zeros([3,self.NPIX])
+        self.phi = self.x * 0.0
         return
 
     def __str__(self):
         return "a model universe, containing potential map phi"
+
+    # ----------------------------------------------------------------
 
     def read_in_CMB_T_map(self,from_this=None):
         if from_this is None:
@@ -79,26 +86,68 @@ class Universe(object):
         hp.mollview(truncated_map,min=-max,max=max)
         return
 
+    # ----------------------------------------------------------------
+
+    def generate_a_random_potential_field(self,nmax=6):
+
+        # Draw unit Gaussian random Fourier coefficients:
+        self.nmax = nmax
+        self.fn = np.array([])
+        self.n = []
+        for nx in range(-self.nmax,self.nmax+1):
+            for ny in range(-self.nmax,self.nmax+1):
+                for nz in range(-self.nmax,self.nmax+1):
+                    if (nx*nx+ny*ny+nz*nz <= self.nmax*self.nmax):
+                        self.n.append([nx,ny,nz])
+                        self.fn = np.append(self.fn,np.random.randn())
+
+        self.n = np.array(self.n)
+        print "Generated ",len(self.fn)," potential Fourier coefficients"
+
+        # Evaluate it on our Phi grid:
+        self.evaluate_potential_given_fourier_coefficients()
+
+        return
+
+
+    def evaluate_potential_given_fourier_coefficients(self):
+
+        deltak = 2.0*np.pi/self.BOXSIZE
+        i = 0
+        for nx in range(-self.nmax,self.nmax+1):
+            for ny in range(-self.nmax,self.nmax+1):
+                for nz in range(-self.nmax,self.nmax+1):
+                    if (nx*nx+ny*ny+nz*nz <= self.nmax*self.nmax):
+                        kx,ky,kz = deltak*self.n[i]
+                        phase = kx * self.x + ky * self.y + kz * self.z
+                        self.phi += self.fn[i]*np.cos(phase)
+                        i += 1
+
+        print " Built potential grid, with dimensions ",self.phi.shape,\
+              " and mean value ", round(np.mean(self.phi)),"+/-",round(np.std(self.phi))
+
+        return
+
 # ====================================================================
 
 """
 Response matrix from Roger's mathematica notebook:
 
 nmax = 6; klst = {};
-Do[If[0 < n1^2 + n2^2 + n3^2 <= nmax^2, 
-  klst = Append[klst, {n1, n2, n3}]], {n1, -nmax, nmax}, {n2, -nmax, 
-  nmax}, {n3, -nmax, nmax}]; NN = 
+Do[If[0 < n1^2 + n2^2 + n3^2 <= nmax^2,
+  klst = Append[klst, {n1, n2, n3}]], {n1, -nmax, nmax}, {n2, -nmax,
+  nmax}, {n3, -nmax, nmax}]; NN =
  Length[klst]; \[CapitalDelta]k = .5 \[Pi];
 lmax = 10; llst = {}; Do[
  If[1 < l <= lmax, llst = Append[llst, {l, m}]], {l, 2, lmax}, {m, -l,
    l}]; llst; L = Length[llst];
 myn = Chop[Table[4. \[Pi] I^llst[[y, 1]]
-     SphericalHarmonicY[llst[[y, 1]], llst[[y, 2]], 
-       ArcCos[klst[[n, 3]]/Norm[klst[[n]]]], 
-       If[klst[[n, 1]] == klst[[n, 2]] == 0, 0, 
-        ArcTan[klst[[n, 1]], 
+     SphericalHarmonicY[llst[[y, 1]], llst[[y, 2]],
+       ArcCos[klst[[n, 3]]/Norm[klst[[n]]]],
+       If[klst[[n, 1]] == klst[[n, 2]] == 0, 0,
+        ArcTan[klst[[n, 1]],
          klst[[n, 2]]]]]\[Conjugate] SphericalBesselJ[
-      llst[[y, 1]], \[CapitalDelta]k Norm[klst[[n]]]], {y, 1, L}, {n, 
+      llst[[y, 1]], \[CapitalDelta]k Norm[klst[[n]]]], {y, 1, L}, {n,
      1, NN}]];
 (*Export["myn.txt",myn]*)
 

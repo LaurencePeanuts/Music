b0aa12939c886735ed4e785ab53ccd6316a88bc6
LaurencePeanuts <laurence.perreaultlevasseur@gmail.com>
2018-07-30 11:56:34 -0700

    fixed the indexing issues with n_max mock and rec not being the same

--- Scripts/_CalcEvidence.py
+++ Scripts/_CalcEvidence.py
@@ -1,29 +1,20 @@
 from numpy.linalg import det
 ind = np.where(beatbox.Universe.kfilter>0)
-PS_long = np.zeros(2*len(ind[1]))
-PS_long[:len(ind[1])] = (beatbox.You.all_simulated_universes[0].Power_Spectrum[ind])
-PS_long[len(ind[1]):] = (beatbox.You.all_simulated_universes[0].Power_Spectrum[ind])
-PS = np.zeros(len(ind[1]))
-PS[:len(ind[1])/2] = PS_long[:len(ind[1])/2]
-PS[len(ind[1])/2:] = PS_long[len(ind[1]):3*len(ind[1])/2]
+fn_length = beatbox.Universe.numfn
+inv_Cf = beatbox.You.calculate_prior_Cf(ind, fn_length)
 
-PiFact = 1./(2.*np.pi)**(len(MockUniverse[i].ay_real)/2.)
-DetDenom = 1./(det(beatbox.You.C_yy)*det(np.diag(PS))*det(beatbox.You.A))**(1./2.)
+PiFact = 1./(2.*np.pi)**(len(true_alms)/2.)
+DetDenom = det(inv_Cf)**(0.5)/(det(beatbox.You.C_yy)*det(beatbox.You.A))**(0.5)
 EvidenceVector = np.zeros(numreal)
 
 for i in range(numreal):
 
-    inv_Cf=np.diag(1./PS)
-    #print det(beatbox.You.C_yy), det(np.diag(PS)), 1./det(beatbox.You.A)
-    #print 1./(det(beatbox.You.C_yy)*det(np.diag(PS))*det(beatbox.You.A))**(1./2.)
-    
-    #print np.exp(0.5* np.dot(MockUniverse[i].ay_real.T , np.dot ( np.dot( beatbox.You.inv_Cyy, np.dot( beatbox.You.R_real, np.dot(beatbox.You.inv_A, np.dot(beatbox.You.R_real.T , beatbox.You.inv_Cyy) ) ) ) - beatbox.You.inv_Cyy , MockUniverse[i].ay_real )  ))
-    NumExp = np.exp(0.5* np.dot(MockUniverse[i].ay_real.T , np.dot ( np.dot( beatbox.You.inv_Cyy, np.dot( beatbox.You.R_real, np.dot(beatbox.You.inv_A, np.dot(beatbox.You.R_real.T , beatbox.You.inv_Cyy) ) ) ) - beatbox.You.inv_Cyy , MockUniverse[i].ay_real )  ))
+    NumExp = np.exp(0.5* np.dot(beatbox.You.all_simulated_universes[i].ay2ayreal_for_inference(beatbox.You.all_simulated_universes[i].ay) , np.dot ( np.dot( beatbox.You.inv_Cyy, np.dot( beatbox.You.R_real, np.dot(beatbox.You.inv_A, np.dot(beatbox.You.R_real.T , beatbox.You.inv_Cyy) ) ) ) - beatbox.You.inv_Cyy , beatbox.You.all_simulated_universes[i].ay2ayreal_for_inference(beatbox.You.all_simulated_universes[i].ay.T) )  ))
     #print 1./(2.*np.pi)**(len(MockUniverse[i].ay_real)/2.)
     EvidenceVector[i] = PiFact * NumExp * DetDenom
 
 
-n, bins, patches = plt.hist(EvidenceVector, np.logspace(-14, 1, 40), normed=0, facecolor='green', alpha=0.75)
+n, bins, patches = plt.hist(EvidenceVector, np.logspace(-50, 1, 40), normed=0, facecolor='green', alpha=0.75)
 
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
@@ -44,15 +35,15 @@ plt.grid(True)
 path_to_save='RobustnessAnalysis/rob_plt_lmax'+str(beatbox.Universe.truncated_lmax)+'_lmin'+str(beatbox.Universe.truncated_lmin)+'_nmax'+str(beatbox.Universe.truncated_nmax)+'_nmin'+str(beatbox.Universe.truncated_nmin)
 
 
-try: 
-    os.makedirs(path_to_save)
-except OSError:
-    if not os.path.isdir(path_to_save):
-        raise    
+#try: 
+#    os.makedirs(path_to_save)
+#except OSError:
+#    if not os.path.isdir(path_to_save):
+#        raise    
 
 
 
-plt.savefig(path_to_save+'/Evidence.png')
+#plt.savefig(path_to_save+'/Evidence.png')
 print np.mean(np.log10(EvidenceVector))
 print np.mean((EvidenceVector))
 
